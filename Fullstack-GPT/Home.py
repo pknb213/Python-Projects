b2f66c65c9f4b1892127f380dda7bb1f126f11e5
cyj <pknb213@naver.com>
2024-04-28 16:55:51 +0900

    다시 LLM 해야지

--- Fullstack-GPT/Home.py
+++ Fullstack-GPT/Home.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from langchain.prompts import PromptTemplate
 
 # streamlit run Home.py
 st.title("Home")
@@ -8,3 +9,14 @@ st.subheader("Welcome to the Home Page")
 st.markdown("""
     #### I love it
 """)
+
+st.write("Hello")
+st.write([1,2,3,4,5])
+st.write({"a": 1, "b": 2, "c":3})
+st.write(PromptTemplate)
+
+p = PromptTemplate.from_template("x x x x")
+st.write(p)
+
+st.selectbox("Select Model", ("GPT-3", "GPT-4", "GPT-5"))
+
