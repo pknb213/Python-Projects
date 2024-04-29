6de677dec37c9a67c9f9b48897de6f916eef6187
cyj <pknb213@naver.com>
2024-04-29 13:15:41 +0900

    [04.29] Document GPT

--- Fullstack-GPT/pages/01_DocumentGPT.py
+++ Fullstack-GPT/pages/01_DocumentGPT.py
@@ -1,38 +1,64 @@
 import time
 
 import streamlit as st
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.embeddings import CacheBackedEmbeddings
+from langchain.storage import LocalFileStore
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores.faiss import FAISS
+from langchain_openai import OpenAIEmbeddings
 
 st.set_page_config(
     page_title="DocumentGPT",
     page_icon="📄"
 )
 
-st.title("DocumentGPT")
 
-if "message" not in st.session_state:
-    st.session_state["messages"] = []
+def embed_fil(file):
+    file_content = file.read()
+    file_path = f"./.cache/files/{file.name}"
+    st.write(file_content, file_path)
 
+    with open(file_path, "wb") as f:
+        f.write(file_content)
 
-def send_message(message, role, save=True):
-    with st.chat_message(role):
-        st.write(message)
-    if save:
-        st.session_state["messages"].append({"role": role, "message": message})
-
-
-for message in st.session_state["messages"]:
-    send_message(
-        message["message"],
-        message["role"],
-        save=False
+    cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
+    splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        separator="\n",
+        chunk_size=600,
+        chunk_overlap=100,
     )
 
-message = st.chat_input("Send a message to the ai")
+    loader = UnstructuredFileLoader(
+        file_path=f"{file_path}"
+    )
+    docs = loader.load_and_split(text_splitter=splitter)
 
-if message:
-    send_message(message, "human")
-    time.sleep(1)
-    send_message(f"You said: {message}", "ai")
+    embeddings = OpenAIEmbeddings()
+    cached_embeddings = CacheBackedEmbeddings.from_bytes_store(
+        embeddings, cache_dir
+    )
+    vectorstore = FAISS.from_documents(docs, cached_embeddings)
+    retriever = vectorstore.as_retriever()
+    return retriever
 
-    with st.sidebar:
-        st.write(st.session_state)
+
+st.title("DocumentGPT")
+
+st.markdown(
+    """
+        Welcome!
+        Use this chatbot to ask questions to an AI about your files!
+    """
+)
+
+file = st.file_uploader(
+    "Upload a .txt, .pdf or .dock file",
+    type=["txt", "pdf", "docx"]
+)
+
+if file:
+    retriever = embed_fil(file)
+    s = retriever.invoke("winston")
+    s
+
