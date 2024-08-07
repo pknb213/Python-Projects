4f7573d78c646df887fb22d2ccd4e5ddb1636ed2
cyj <pknb213@naver.com>
2024-08-07 17:34:56 +0900

    [08.07] Site_GPT

--- Fullstack-GPT/pages/04_SiteGPT.py
+++ Fullstack-GPT/pages/04_SiteGPT.py
@@ -2,10 +2,55 @@
 # 2. sitemap
 import streamlit as st
 from bs4 import BeautifulSoup
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import SitemapLoader
+from langchain_community.vectorstores import FAISS
+# from langchain_community.chat_models import ChatOpenAI
+from langchain_openai import ChatOpenAI
+# from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings
+
+llm = ChatOpenAI(
+    temperature = 0.1,
+)
+
+answers_prompt = ChatPromptTemplate.from_template("""
+    Using ONLY the following context answer the user's question. If you can't just say you don't know, don't make anything up.
+                                                  
+    Then, give a score to the answer between 0 and 5.
+    If the answer answers the user question the score should be high, else it should be low.
+    Make sure to always include the answer's score even if it's 0.
+    Context: {context}
+                                                  
+    Examples:
+                                                  
+    Question: How far away is the moon?
+    Answer: The moon is 384,400 km away.
+    Score: 5
+                                                  
+    Question: How far away is the sun?
+    Answer: I don't know
+    Score: 0
+                                                  
+    Your turn!
+    Question: {question}
+""")
 
 # ua = UserAgent()
+
+def get_answers(inputs):
+    docs = inputs["docs"]
+    question = inputs["question"]
+    answers_chain = answers_prompt | llm
+    answers = []
+    for doc in docs:
+        result = answers_chain.invoke(
+            {"question": question, "context": doc.page_content}
+        )
+        answers.append(result.content)
+    st.write(answers)
 
 
 def parse_page(soup: BeautifulSoup):
@@ -23,7 +68,7 @@ def parse_page(soup: BeautifulSoup):
     )
 
 
-@st.cache_data(show_spinner="Loading Website...")
+@st.cache_resource(show_spinner="Loading Website...")
 def load_website(url):
     splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
         chunk_size=1000,
@@ -31,21 +76,23 @@ def load_website(url):
     )
     loader = SitemapLoader(
         url,
-        filter_urls=[
-            # r"^(.*\/blog\/).*",
-        ],
+        # filter_urls=[
+        #     r"^(.*\/blog\/).*",
+        # ],
         parsing_function=parse_page,
     )
-    loader.requests_per_second = 5
-    loader.headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-    }
+    loader.requests_per_second = 2
+    # loader.headers = {
+    #     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+    # }
     docs = loader.load_and_split(text_splitter=splitter)
-    return docs
+    st.write(docs)
+    vector_store = FAISS.from_documents(docs, OpenAIEmbeddings())
+    return vector_store.as_retriever()
 
 
 st.set_page_config(
-    page_title="Quiz GPT",
+    page_title="Site GPT",
     page_icon="？",
 )
 
@@ -68,5 +115,11 @@ if url:
         with st.sidebar:
             st.error("Please provide a sitemap URL")
     else:
-        docs = load_website(url)
-        st.write(docs)
+        retriever = load_website(url)
+
+        chain = {
+            "docs": retriever,
+            "question": RunnablePassthrough(),
+        } | RunnableLambda(get_answers)
+
+        chain.invoke("What is the pricing of GPT-4 Turbo with vision.")
