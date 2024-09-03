3aabcbec86f828881b9645f47f6e6e15d651dbcf
cyj <pknb213@naver.com>
2024-09-03 16:34:24 +0900

    [09.03] The End
    - Lecture end
    - remain chapter
      1. Using AWS, Azure model
      2. Using crewai framework
      3. Using Not Free Function

--- Fullstack-GPT/main.py
+++ Fullstack-GPT/main.py
@@ -1,13 +1,31 @@
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
 
-app = FastAPI()
+app = FastAPI(
+    title="Nicolacus Maximus Quote Giver",
+    description="Get a real quote said by Nicolacus Maximus himself.",
+)
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+
+class Quote(BaseModel):
+    quote: str = Field(
+        description="The quote that Nicolacus Maximus said.",
+    )
+    year: int = Field(
+        description="The year when Nicolacus Maximus said the quote.",
+    )
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get(
+    "/quote",
+    summary="Returns a random quote by Nicolacus Maximus",
+    description="Upon receiving a GET request this endpoint will return a real quiote said by Nicolacus Maximus himself.",
+    response_description="A Quote object that contains the quote said by Nicolacus Maximus and the date when the quote was said.",
+    response_model=Quote,
+)
+def get_quote():
+    return {
+        "quote": "Life is short so eat it all.",
+        "year": 1950,
+    }
